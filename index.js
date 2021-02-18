a141865d1aa21c2bb11649dc65b56139c86411d8
Damokles <damokless@protonmail.com>
2021-02-18 19:17:54 +0100

    fixing IA

--- index.js
+++ index.js
@@ -132,21 +132,15 @@ recursiveAsyncReadLine(line1, line2, line3, line4) // we have to actually start 
 
 async function iaTurns (line1, line2, line3, line4) {
   const randomLine = Math.floor(Math.random() * (5 - 1)) + 1
+  switch (randomLine) {
+    case 1:
+
+      break
+
+    default:
+      break
+  }
   const randomMatch = Math.floor(Math.random() * (8 - 1)) + 1
-  console.log('* * * * * * * * ')
-  console.log(
-    `*${line1[0]} ${line1[1]} ${line1[2]} ${line1[3]} ${line1[4]} ${line1[5]} ${line1[6]}*`
-  )
-  console.log(
-    `*${line2[0]} ${line2[1]} ${line2[2]} ${line2[3]} ${line2[4]} ${line2[5]} ${line2[6]}*`
-  )
-  console.log(
-    `*${line3[0]} ${line3[1]} ${line3[2]} ${line3[3]} ${line3[4]} ${line3[5]} ${line3[6]}*`
-  )
-  console.log(
-    `*${line4[0]} ${line4[1]} ${line4[2]} ${line4[3]} ${line4[4]} ${line4[5]} ${line4[6]}*`
-  )
-  console.log('* * * * * * * * ')
   console.log("AI's turn ...")
 
   switch (randomLine) {
---
+++ test.js
@@ -0,0 +1,4 @@
+const names = [' ', '|', ' ', '|']
+const results = names.filter(x => x.includes('|'))
+console.log(results.length)
+console.log(typeof (results))
